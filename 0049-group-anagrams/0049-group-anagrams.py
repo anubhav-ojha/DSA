c02e7f1e4e3a486ec78a4524b9590c421a60ab4b
Anubhav ojha <anubhavojha9@gmail.com>
2023-07-04 08:01:23 +0530

    Time: 136 ms (18.56%), Space: 22.3 MB (9.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,8 +3,11 @@ class Solution:
         res = defaultdict(list)
         
         for s in strs:
-            count = [0] * 26  #initial a ...z
+            
+            count = [0] * 26
             for c in s:
-                count[ord(c)-ord('a')] += 1
-            res[tuple(count)].append(s)
-        return res.values()
+                count[ord(c) - ord('a')] += 1
+                
+            res[tuple(count)].append(s)   
+        return res.values()    
+        
