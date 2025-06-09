9ee15f2014bdfd21de49eb5a3e335349a41bba4f
Anubhav ojha <anubhavojha9@gmail.com>
2025-06-09 15:27:40 +0530

    Time: 4 ms (4.98%), Space: 17.8 MB (64.68%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def findKthNumber(self, n, k):
+        curr = 1
+        k -= 1
+
+        while k > 0:
+            step = self._count_steps(n, curr, curr + 1)
+            # If the steps are less than or equal to k, we skip this prefix's subtree
+            if step <= k:
+                # Move to the next prefix and decrease k by the number of steps we skip
+                curr += 1
+                k -= step
+            else:
+                # Move to the next level of the tree and decrement k by 1
+                curr *= 10
+                k -= 1
+
+        return curr
+
+    # To count how many numbers exist between prefix1 and prefix2
+    def _count_steps(self, n, prefix1, prefix2):
+        steps = 0
+        while prefix1 <= n:
+            steps += min(n + 1, prefix2) - prefix1
+            prefix1 *= 10
+            prefix2 *= 10
+        return steps
